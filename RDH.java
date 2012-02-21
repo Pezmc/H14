b7ac5895b333bd2c3d77cd9c299dd4ee76796fbb
Pez Cuckow <email@pezcuckow.com>
2012-02-21 14:27:25 +0000

    Added print methods

--- RDH.java
+++ RDH.java
@@ -1,25 +1,5 @@
 /* This class needs */
 
-/* T5: Plan the implementation (10 minutes):  Use the Java programming language to implement the  design 
-obtained in T3 and T4 into a working system.
-
-This system, to be called “IBMS-RDH”, is a subsystem of the 
-IBMS. Since each team will build your own IBMS-RDH, I suggest that you name your team’s IBMS-RDH as 
-“teamID- IBMS-RDH”
-
-For example, H3-IBMS-RDH is for team H3; G5-IBMS-RDH is for team G5, and 
-I8-IBMS-RDH is for team I8. Before starting implementation, you should plan the implementation tasks and 
-decide who does what by when. The main implementation tasks are: 
-  T5-1: Implement the design classes; 
-  T5-2: Implement the user interface; 
-  T5-3: Connect the design classes to the IBMS bus database; 
-  T5-4: Test the system (hint: test examples can be used for demonstration in Week 4).
-
-The interface should be interactive such that it will ask the user to enter the holiday request information and 
-display the approved holidays on the screen
-
-*/
-
 /*
 
 4.	A completely working system for UC1 is defined as follows:
@@ -27,7 +7,6 @@ a.	Its interface is interactive: it asks for the input and displays the output, 
 b.	Given the driver ID as the input, the interface shows the number of holidays that the driver has.
 c.	The driver selects specific dates for the holidays and the interface will display one of the two outcomes: (1) holidays granted; (2) holidays rejected due to invalid dates. 
 d.	The above operations should be repeatable without crashing the system.
-
 
 */
 
@@ -60,7 +39,8 @@ RELATED RULES
 12.	If a driver requests holidays, he or she should specify the intended starting date and the finishing date.
 */
 
-public class RDH {
+/*
+Basic workflow
   //Prompt user for driver ID
     //Display holidays driver has at the moment
   //Allow user to request new holidays 
@@ -68,6 +48,15 @@ public class RDH {
     //Holiday is accepted or rejected due to invalid dates
 
   //Don't crash and allow go back to start
+*/
+
+
+/**
+ * Class used with a CLI to request a driver holiday
+ * Takes driver ID and requests holidays
+ */
+public class RDH {
+
   
   // Buffered reader to grab input
   static BufferedReader input =
@@ -91,10 +80,26 @@ public class RDH {
   }
   
   /**
+   * Print out a line (alias)
+   */
+  private static void print(String message) {
+    System.out.print(message);
+  }
+  
+  /**
+   * Print out information to the user (alias)
+   */
+  private static void println(String message) {
+    System.out.println(message);
+  }
+  
+  /**
    * @param args the command line arguments
    */
   public static void main(String[] args) {
       database.openBusDatabase();
+       
+       
        
       //Greet
       //while not done loop
