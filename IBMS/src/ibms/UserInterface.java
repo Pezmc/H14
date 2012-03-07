84326def197952a613b5cc2cc5956f55e227c868
Ben Edwards <benedwards01@hotmail.co.uk>
2012-03-07 11:26:55 +0000

    user interface

---
+++ IBMS/src/ibms/UserInterface.java
@@ -0,0 +1,19 @@
+
+package ibms;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+/**
+ *
+ * @author Ben
+ */
+public class UserInterface extends javax.swing.JFrame {
+
+    public UserInterface() {
+
+    }
+    
+
+}
